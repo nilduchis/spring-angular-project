8e6a54c94ebaf72224ecd2530921f11a2f927245
Nilda <nildayatzyry@gmail.com>
2022-04-25 21:16:33 -0500

    spring-angular-project

---
+++ src/app/clientes/clientes.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html'
+})
+export class ClientesComponent implements OnInit {
+
+clientes: Cliente[];
+
+  constructor(private clienteService: ClienteService) { }
+
+  ngOnInit(){
+    this.clienteService.getClientes().subscribe(
+      clientes => this.clientes = clientes
+    );
+  }
+
+  delete(cliente: Cliente): void{
+    const swalWithBootstrapButtons = swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+
+  swalWithBootstrapButtons.fire({
+    title: 'Está seguro?',
+    text: `¿Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Si, eliminar!',
+    cancelButtonText: 'No, cancelar!',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+
+      this.clienteService.delete(cliente.id).subscribe(
+        response => {
+          this.clientes = this.clientes.filter(cli => cli !== cliente)
+      swalWithBootstrapButtons.fire(
+        'Cliente eliminado!',
+        `Cliente ${cliente.nombre} eliminado con éxito.`,
+        'success'
+      )
+  }
+)
+  }
+
+})
+}}
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,7 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Andres', apellido: 'Guzman', email: 'profe@correo.com', createAt: '2022-03-21'},
+  {id: 2, nombre: 'Jossue', apellido: 'Loubet', email: 'jossue@correo.com', createAt: '2022-03-21'},
+  {id: 3, nombre: 'Nilda', apellido: 'Flores', email: 'nilda@correo.com', createAt: '2022-03-21'}
+];
